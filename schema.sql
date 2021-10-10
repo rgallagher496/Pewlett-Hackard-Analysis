c5fb1906e1948aece25aa8504936272cdd65f0d5
Robert Gallagher <rgallagher496@gmail.com>
2021-10-09 21:26:14 -0400

    Created retirement info table and csv file

--- schema.sql
+++ schema.sql
@@ -36,14 +36,14 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no), 
-	PRIMARY KEY (dept_no,emp_no)
+	PRIMARY KEY (emp_no,dept_no)
 );
 
 CREATE TABLE titles (
